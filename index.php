a728f12a4e3b3a2723a71d2c4dbea9e70040ca87
Paulo <paulo@hotmail.com>
2024-03-24 19:17:41 -0300

    .

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php
+include 'includes/header.php';
+
+?>
+    <div class="container">
+
+        <div class="row justify-content-md-center">
+            <div class="d-grid gap-2 col-6 mx-auto">
+                <a href="https://www.facebook.com" class="btn btn-outline-primary btn-lg mb-3">
+                    <i class="fab fa-facebook-square mr-2"></i>
+                    Facebook
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="d-grid gap-2 col-6 mx-auto">
+            <a href="https://www.twitter.com" class="btn btn-info btn-lg btn-block mb-3">
+                    <i class="fab fa-twitter-square mr-2"></i>
+                    Twitter
+                </a>
+            </div>
+        </div>
+
+    </div>
+<?php
+include 'includes/footer.php';
