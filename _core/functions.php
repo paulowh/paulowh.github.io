be5d1ad98d6a5e7c1c9ce2596e9cee55b4730c03
Paulo <paulo@hotmail.com>
2024-05-11 22:24:54 -0300

    remove file

--- _core/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function publicPath($path = '')
-{
-    $path = str_replace(':80', '', $path);
-    $public_path = substr('../', 0, -1);
-    return $public_path . $path;
-}
--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-
-</body>
-
-<script src="assets/js/jquery-3.7.1.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-</html>
