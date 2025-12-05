13421aa6d0bd753725fa14d6c04edd8a8dc1c554
Paulo Santos <ph744915@gmail.com>
2025-12-05 10:53:51 -0300

    fix: corrigindo grid de loading img

--- js/loading.js
+++ js/loading.js
@@ -3,27 +3,25 @@ document.addEventListener('DOMContentLoaded', function () {
   const preloader = document.querySelector('.preloader');
   const postloader = document.querySelector('.postloader');
   const gridTemplate = document.querySelector('.grid-template');
-
+  const gridImg = document.querySelector('.grid-img');
 
   postloader.style.display = 'none';
 
-
   setTimeout(() => {
+    const targetRect = gridImg.getBoundingClientRect();
 
-    const targetRect = document.querySelector('.grid-img').getBoundingClientRect();
-
-
-    preloader.style.position = 'absolute';
     preloader.style.top = targetRect.top + 'px';
     preloader.style.left = targetRect.left + 'px';
     preloader.style.width = targetRect.width + 'px';
     preloader.style.height = targetRect.height + 'px';
     preloader.style.transform = 'none';
 
-
     setTimeout(() => {
+      preloader.style.opacity = '0';
+      preloader.style.pointerEvents = 'none';
+      postloader.style.display = 'block';
+      postloader.classList.add('visible');
       gridTemplate.classList.add('loaded');
-    
     }, 1200); // (deve coincidir com o CSS)
-  }, 150); 
+  }, 150);
 });
